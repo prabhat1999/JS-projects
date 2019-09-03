b8d415a517e368a55273800a6f5da08fa8190e76
unknown <prabhat.avb@gmail.com>
2019-09-04 00:44:41 +0530

    delete functionality

--- todo/todo.js
+++ todo/todo.js
@@ -3,20 +3,46 @@ console.log("connected!");
 var response;
 const arr = [];
 var toDoItem;
+var delindex;
 
-//response = prompt("What would u like to do?");
 do{
     response = prompt("What would u like to do?");
 
 switch (response){
     case "new":
-        toDoItem = prompt("Enter your item");
-        arr.push(toDoItem);
+        addToDo();
         break;
     case "list":
-        console.log(arr);
+        //console.log(arr);
+       listToDo();
         break;
+    case "delete":
+        deleteToDo();
     case "quit":
         break;
     }
 }while(response !== "quit");
+
+function addToDo(){
+    toDoItem = prompt("Enter your item");
+    arr.push(toDoItem);
+    console.log("added todo");
+}
+
+function listToDo(){
+
+    var n = 0;
+    console.log("*******");
+    arr.forEach(function(ele){
+        console.log(n+": "+ ele);
+        n++;
+    });
+    console.log("*******");
+}
+
+function deleteToDo(){
+
+    delindex = prompt("Enter the index to be delete a todo.");
+    arr.splice(delindex,1);
+    console.log("Deleted todo");
+}
