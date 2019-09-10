a034f7071bff1b2964451c3c77d4e5684617a718
unknown <prabhat.avb@gmail.com>
2019-09-11 01:02:45 +0530

    updated colortogg;

--- colortogg/color.js
+++ colortogg/color.js
@@ -1,13 +1,13 @@
 var butt = document.querySelector("button");
 
 butt.addEventListener("click",function(){
-    console.log("Button clicked!!");
-    
-    if(document.body.style.background == "cyan"){
+/*    if(document.body.style.background == "cyan"){
         document.body.style.background = "white";
     }
     else
     {
         document.body.style.background = "cyan";
-    }
+    } */
+
+    document.body.classList.toggle("bg-cyan");
 });
